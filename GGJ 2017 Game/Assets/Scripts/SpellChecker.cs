8538533865ebbc8a115fd59c203661ef71c56e02
HardCode Lab <accounts@hardcodelab.com>
2017-01-20 20:40:01 +0000

    LeapMotion hands now interact with Unity GameObjects in-scene

---
+++ GGJ 2017 Game/Assets/Scripts/SpellChecker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellChecker : MonoBehaviour {
+
+	public GameObject FrontSpellChecker;
+
+	public bool spellDone = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.Equals(FrontSpellChecker))
+		{
+			if (!spellDone)
+			{
+				Debug.Log("Touching the " + other.name);
+				spellDone = true;
+			}
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		spellDone = false;
+	}
+}
