672e580d67a00f9735f0787a1e092ead163e74fa
HardCode Lab <accounts@hardcodelab.com>
2017-01-21 16:52:23 +0000

    New Monster Added: Skeleton
    Skeleton added with Nav Mesh agent and Animation already attached. No attacking yet. Skeleton does not react to spells yet.

---
+++ GGJ 2017 Game/Assets/Scripts/MonsterAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class MonsterAI : MonoBehaviour {
+
+	public bool isAlive = true;
+	public Transform player;
+
+	NavMeshAgent agent;
+	Animator animator;
+
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () 
+	{
+		if (isAlive)
+		{
+			agent.SetDestination(player.position);
+			
+			animator.SetFloat("speed", agent.velocity.magnitude);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+	}
+}
