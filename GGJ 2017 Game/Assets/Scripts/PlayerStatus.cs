c9b00fcda7b86f5445e41e0d487ecad87e62e74d
Lanaya <lanaya21261@gmail.com>
2017-01-22 13:23:39 +0000

    12312asfd

---
+++ GGJ 2017 Game/Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatus : MonoBehaviour {
+
+	public bool isAlive = true;
+	public Image healthBar;
+	public float CurrentHealth = 500;
+	public float MaxHealth = 500;
+	
+	float HealthPercentage;
+
+	void Awake()
+	{
+		CurrentHealth = MaxHealth;
+	}
+
+	void Update () 
+	{
+		HealthPercentage = CurrentHealth/MaxHealth;
+		healthBar.fillAmount = HealthPercentage;
+
+		isAlive = (HealthPercentage > 0);
+
+		Debug.Log(CurrentHealth);
+	}
+}
